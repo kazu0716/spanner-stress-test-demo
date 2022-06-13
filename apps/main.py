fd589f25008fece65333d6ad999f3b9cb3259c4a
kazu0716 <kazu2055.tennis@gmail.com>
2022-06-14 00:46:17 +0900

    add: user create

--- apps/main.py
+++ apps/main.py
@@ -3,8 +3,3 @@ from routers.users import router as user_router
 
 app = FastAPI()
 app.include_router(user_router, prefix="/api/v1")
-
-
-@app.get("/health_check")
-def health_check():
-    return {"status": "running"}
--- apps/routers/users.py
+++ apps/routers/users.py
@@ -1,10 +1,29 @@
 from fastapi import APIRouter
+from pydantic import BaseModel, EmailStr, SecretStr, Field
 
 router = APIRouter(
     prefix="/users",
     tags=["users"],
 )
 
+class User(BaseModel):
+    name: str
+    mail: EmailStr
+    password: SecretStr = Field(min_length=8, max_length=16)
+
+
 @router.get("/", tags=["users"])
-def read_users():
+def read_user():
+    """
+    Get user info
+    """
+    # TODO: get user info from Cloud Spanner
     return [{"username": "Rick"}, {"username": "Morty"}]
+
+@router.post("/", tags=["users"])
+def create_user(user: User):
+    """
+    Create user
+    """
+    # TODO: create user and store it to Cloud Spanner
+    return user
